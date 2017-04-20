2e2c305de5649fb6d563fbc0f94e7dc03978787a
WangZhiguo <ieinstein@163.com>
2017-04-20 18:55:55 +0800

    修改 spring-mvc.xml配置

--- src/main/java/com/river/toutiao/controller/IndexController.java
+++ src/main/java/com/river/toutiao/controller/IndexController.java
@@ -1,8 +1,10 @@
 package com.river.toutiao.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.rmi.RemoteException;
+import java.util.List;
 
+import org.decaywood.collector.MostProfitableCubeCollector;
+import org.decaywood.entity.Cube;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,12 +13,17 @@ import com.alibaba.fastjson.JSONObject;
 
 @Controller
 public class IndexController {
-	@RequestMapping(value="/c/v1/index")
-	@ResponseBody
-	public String index() {
-		Map<String, Object> map= new HashMap<>();
-		map.put("check", "OK");
-		map.put("code", 0);
-		return JSONObject.toJSONString(map);
-	}
+    @RequestMapping(value = "/c/v1/index")
+    @ResponseBody
+    public String index() throws RemoteException {
+        MostProfitableCubeCollector collector = new MostProfitableCubeCollector();
+        List<Cube> cubes = collector.get();
+        return JSONObject.toJSONString(cubes, true);
+    }
+
+    public static void main(String[] args) throws RemoteException {
+        MostProfitableCubeCollector collector = new MostProfitableCubeCollector();
+        List<Cube> cubes = collector.get();
+        cubes.forEach((Cube cube) -> System.out.println(JSONObject.toJSONString(cube)));
+    }
 }
